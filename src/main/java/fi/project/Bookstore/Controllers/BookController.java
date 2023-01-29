279637082efebb6023750aff74116ae8060b011c
SProkopios <s.koponen93@gmail.com>
2023-01-29 13:20:40 +0200

    First commit

---
+++ src/main/java/fi/project/Bookstore/Controllers/BookController.java
@@ -0,0 +1,14 @@
+package fi.project.Bookstore.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	
+	@GetMapping("/index")
+	public String hello() {
+		return "";
+	}
+}
