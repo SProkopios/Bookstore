16b0b22d2a91f3c082034ff31e1402d2c7ce0cb5
SProkopios <s.koponen93@gmail.com>
2023-02-03 23:40:48 +0200

    Adding Crud functionalities

--- src/main/java/fi/project/Bookstore/domain/Book.java
+++ src/main/java/fi/project/Bookstore/domain/Book.java
@@ -11,7 +11,7 @@ public class Book {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private Long id;
 	
 	private String title;
 	private String author;
@@ -34,7 +34,7 @@ public class Book {
 		return id;
 	}
 	
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	
--- src/main/java/fi/project/Bookstore/web/BookController.java
+++ src/main/java/fi/project/Bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.project.Bookstore.domain.Book;
 import fi.project.Bookstore.domain.BookRepository;
 
 @Controller
@@ -13,10 +16,28 @@ public class BookController {
 
 	@Autowired
 	private BookRepository repository;
-	
+
 	@RequestMapping("/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@RequestMapping(value = "/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
